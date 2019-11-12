819fcf4ac48b4dc656d010ab04183202453a1c3d
samog <samog@10.1.190.57>
2019-11-11 21:38:42 -0600

    f3

--- myapplication/src/main/java/com/example/myapplication/Functions.java
+++ myapplication/src/main/java/com/example/myapplication/Functions.java
@@ -78,10 +78,10 @@ public class Functions {
 		Statement st = dbConnection.createStatement();
 		st.executeQuery("use mtconnect");
 		
-		ResultSet results = st.executeQuery("SELECT * FROM user_m WHERE m_number ='"+mNumber+"' AND pw ='"+password+ "'");
+		ResultSet results = st.executeQuery("SELECT * FROM user_m WHERE m_number ='"+mNumber+"' AND PW ='"+password+ "'");
 		results.next();
 		
-		if( (results.getString("m_number") == mNumber) && (results.getString("pw") == password))
+		if( (results.getString("m_number") == mNumber) && (results.getString("PW") == password))
 		{	
 			DataClasses.User pulledUser = new DataClasses.User(
 				results.getString("name_f"),
