4d9bdbafd8a4c40e4a10c3f1198bea4b0ee08707
codymaness <Kodie_030299>
2019-12-04 17:49:54 -0600

    Added picture and correct formatting

--- myapplication/src/main/java/com/example/myapplication/HomeView.java
+++ myapplication/src/main/java/com/example/myapplication/HomeView.java
@@ -12,6 +12,7 @@ import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.DateFormat;
@@ -24,6 +25,17 @@ public class HomeView extends AbsoluteLayout implements View {
     public HomeView(Navigator navigator) throws SQLException {
 
         Connection dbConnection = MySqlCon.connect();
+
+        String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+        FileResource resource = new FileResource(new File(basepath + "/WEBINF/images/Hackers-Choice-Award72-1170x780.jpg"));
+        Image image = new Image("Image from file",resource);
+        image.setHeight("100%");
+        image.setWidth("100%");
+        HorizontalLayout imageLayout = new HorizontalLayout();
+        imageLayout.addComponent(image);
+        imageLayout.setWidth("100%");
+        imageLayout.setHeight("100%");
+        addComponent(imageLayout, "right: 10%; left: 10%; bottom: 50%");
 
         /* ----- Title Bar ----- */
         HorizontalLayout titleBar = new HorizontalLayout();
@@ -77,17 +89,22 @@ public class HomeView extends AbsoluteLayout implements View {
 
 
         /* ----- GRID LAYOUT -----*/
-        GridLayout events = new GridLayout(4,1);
+        GridLayout events = new GridLayout(1,10);
         events.setWidth("100%");
-        addComponent(events, "top: 50%; left: 20%; right: 20%");
+        addComponent(events, "top: 55%; left: 20%; right: 20%");
         ArrayList<DataClasses.Event> eventList = Functions.pullEventArray(dbConnection);
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
         for(int x = 0; x < 5; x++) {
             HorizontalLayout eventLine = new HorizontalLayout();
             Label title = new Label(eventList.get(x).getEvent_name());
+            Label date = new Label("Date");
             //Label date = new Label(dateFormat.format(eventList.get(x).getDate_of_event()));
             eventLine.addComponents(title);
+            eventLine.addComponents(date);
+            eventLine.setComponentAlignment(title, Alignment.MIDDLE_RIGHT);
+            eventLine.setComponentAlignment(title, Alignment.MIDDLE_LEFT);
+            eventLine.setWidth("80%");
             eventLine.addLayoutClickListener(event -> {
                 Window description = new Window("Event Description");
                 VerticalLayout windowContent = new VerticalLayout();
@@ -112,39 +129,5 @@ public class HomeView extends AbsoluteLayout implements View {
             });
             events.addComponents(eventLine);
         }
-
-
-        // Packaging up one poster
-
-        VerticalLayout eventpackage2 = new VerticalLayout();
-        eventpackage2.addLayoutClickListener(event -> {
-            Window description = new Window("Event Description");
-            VerticalLayout windowContent = new VerticalLayout();
-            description.setContent(windowContent);
-            description.setModal(true);
-            description.center();
-            description.setWidth("390px");
-            description.setHeight("400px");
-
-            Label text = new Label("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed " +
-                    "do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
-                    "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure " +
-                    "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " +
-                    "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est " +
-                    "laborum.</p>");
-            text.setContentMode(ContentMode.HTML);
-            text.setWidth("400px");
-            windowContent.addComponent(text);;
-
-
-            UI.getCurrent().addWindow(description);
-        });
-
-
-
-
-
-
-
     }
 }
